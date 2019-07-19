3c6f742239093a50d86314f5ad14e42ab158ef31
lincerely <lincerely@gmail.com>
2019-07-20 05:24:38 +0800

    reformat

--- todo.hs
+++ todo.hs
@@ -19,21 +19,20 @@ data Command = CmdAdd String
              deriving (Eq, Show, Read)
 
 issue :: Command -> [Todo] -> Either String Action
-issue (CmdAdd newContent) ts = Right $ Add (Todo (length ts) newContent Pending)
+issue (CmdAdd newContent) [] = Right $ Add (Todo 0 newContent Pending)
+issue (CmdAdd newContent) ts =
+    Right $ Add (Todo (idx (last ts) + 1) newContent Pending)
 issue (CmdDelete targetID) ts =
     let targetTodo = find (\t -> idx t == targetID) ts
-     in case targetTodo of
-          Nothing -> 
-              Left $ "todo with id " ++ show targetID ++ " not found"
-          Just t  -> 
-              Right $ Delete t
+    in  case targetTodo of
+            Nothing -> Left $ "todo with id " ++ show targetID ++ " not found"
+            Just t  -> Right $ Delete t
 issue (CmdUpdate targetID newState) ts =
     let targetTodo = find (\t -> idx t == targetID) ts
-     in case targetTodo of
-          Nothing -> 
-              Left $ "todo with id " ++ show targetID ++ " not found"
-          Just (Todo _ _ oldState) -> 
-              Right $ UpdateState targetID oldState newState
+    in  case targetTodo of
+            Nothing -> Left $ "todo with id " ++ show targetID ++ " not found"
+            Just (Todo _ _ oldState) ->
+                Right $ UpdateState targetID oldState newState
 issue (CmdUndo action) _ = Right $ Undo action
 
 data Action = Add Todo
@@ -43,23 +42,23 @@ data Action = Add Todo
             deriving (Eq, Show, Read)
 
 apply :: Action -> [Todo] -> [Todo]
-apply (Add t)                        = (++[t])
-apply (Delete t)                     = delete t
+apply (Add    t                    ) = (++ [t])
+apply (Delete t                    ) = delete t
 apply (UpdateState todoID oldS newS) = updateTodoState todoID oldS newS
-apply (Undo action)                  = apply $ undo action
+apply (Undo action                 ) = apply $ undo action
 
 constructList :: [Action] -> [Todo]
 constructList = foldr apply []
 
 updateTodoState :: Int -> TodoState -> TodoState -> [Todo] -> [Todo]
 updateTodoState _ _ _ [] = []
-updateTodoState todoID oldState newState  (t:ts)
-  | todoID == i =  Todo i c newState : ts
-  | otherwise = t : updateTodoState todoID oldState newState ts
-  where (Todo i c _) = t
+updateTodoState todoID oldState newState (t : ts)
+    | todoID == i = Todo i c newState : ts
+    | otherwise   = t : updateTodoState todoID oldState newState ts
+    where (Todo i c _) = t
 
 undo :: Action -> Action
-undo (Add t)                    = Delete t
-undo (Delete t)                 = Add t
+undo (Add    t                ) = Delete t
+undo (Delete t                ) = Add t
 undo (UpdateState todoID s1 s2) = UpdateState todoID s2 s1
-undo (Undo act)                 = act
+undo (Undo act                ) = act
